0e82544a260df00478bb496f49326325f8f72d69
Steve Chien <steve@productlab.com>
2013-02-10 09:20:44 -0500

    changes

--- src/main/java/com/esoteric/hadoop/sample/AvroTryout.java
+++ src/main/java/com/esoteric/hadoop/sample/AvroTryout.java
@@ -8,10 +8,14 @@ import org.apache.avro.mapred.AvroKey;
 import org.apache.avro.mapreduce.AvroJob;
 import org.apache.avro.mapreduce.AvroKeyInputFormat;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -63,18 +67,29 @@ public class AvroTryout extends Configured implements Tool {
 	 * @see org.apache.hadoop.util.Tool#run(java.lang.String[])
 	 */
 	@Override
-	public int run(String[] arg0) throws Exception {
+	public int run(String[] args) throws Exception {
 		Job job = new Job(getConf(), AvroTryout.class.getName());
+		
+		job.setJarByClass(getClass());
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		
 	    job.setInputFormatClass(AvroKeyInputFormat.class);
 	    AvroJob.setInputKeySchema(job, SCHEMA);
 	    
+	    job.setSpeculativeExecution(false);
+	    job.setOutputFormatClass(SequenceFileOutputFormat.class);
+	    
 	    job.setMapperClass(MyAvroMapper.class);
 	    job.setMapOutputKeyClass(Text.class);
 	    job.setMapOutputValueClass(Text.class);
-	   
+	    
 	    /* This job does not need any reducer */
 	    job.setNumReduceTasks(0);
+	    
+	    job.setOutputKeyClass(Text.class);
+	    job.setOutputValueClass(Text.class);
 	    
 	    return job.waitForCompletion(true) ? 1 : 0;
 	}
